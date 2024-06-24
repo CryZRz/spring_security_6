1a73ac195d5c33d8c3ac41e1db745969c0ff420b
cryz <cryzrz@github.com>
2024-06-23 20:52:53 -0600

    01 bassics :)

---
+++ src/main/java/com/cryz/app_security/controllers/BalanceController.java
@@ -0,0 +1,17 @@
+package com.cryz.app_security.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/balance")
+public class BalanceController {
+    @GetMapping
+    public Map<String, String> balance(){
+        return Collections.singletonMap("msj", "balance");
+    }
+}
